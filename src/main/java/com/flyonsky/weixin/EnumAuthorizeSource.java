2b9301f7c7a8765f0674cbe6a17e003d74657013
luowg <wengang.luo@cocopico.com>
2016-05-20 10:57:50 +0800

    添加新的枚举类型

---
+++ src/main/java/com/flyonsky/weixin/EnumAuthorizeSource.java
@@ -0,0 +1,25 @@
+package com.flyonsky.weixin;
+
+/**
+ * 微信请求授权的来源
+ * @author Administrator
+ *
+ */
+public enum EnumAuthorizeSource {
+	/**
+	 * 移动应用开发
+	 */
+	APP,
+	/**
+	 * 网站应用开发
+	 */
+	WEB,
+	/**
+	 * 公众帐号开发
+	 */
+	SERVICE,
+	/**
+	 * 公众号第三方平台开发
+	 */
+	THIRD_PLATFORM
+}
