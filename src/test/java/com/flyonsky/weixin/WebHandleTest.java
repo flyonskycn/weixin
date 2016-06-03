e17721c319a297f39f01d2475bd27e0227ec4de1
luowg <wengang.luo@cocopico.com>
2016-06-03 16:35:35 +0800

    增加测试用例

--- src/test/java/com/flyonsky/weixin/WebHandleTest.java
+++ src/test/java/com/flyonsky/weixin/WebHandleTest.java
@@ -3,6 +3,7 @@ package com.flyonsky.weixin;
 import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 
 import com.flyonsky.BaseSpringJUnit;
 import com.flyonsky.weixin.data.EnumScope;
@@ -13,13 +14,20 @@ import com.flyonsky.weixin.data.EnumScope;
  *
  */
 public class WebHandleTest extends BaseSpringJUnit{
+	
+	// web应用appid
+	@Value("${weixin.coco.web.appid}")
+	private String appId;
 
 	@Autowired
 	private WebHandle service;
 	
+	/**
+	 * 获取微信网页授权地址
+	 */
 	@Test
-	public void testWebAuthorize(){
-		String url = this.getService().authorizeUrl("wx30ce83aa5b2991f3", EnumScope.snsapi_userinfo, "www.cocopico.com");
+	public void testAuthorizeUrl(){
+		String url = this.getService().authorizeUrl(appId, EnumScope.snsapi_login, "www.cocopico.com");
 		System.out.println(url);
 		Assert.assertNotNull(url);
 	}
